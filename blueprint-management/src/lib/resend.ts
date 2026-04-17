e0de463dc7a5295ac039a81be330e78658ce4176
Darin Butler <butlerdarin@gmail.com>
2026-04-17 20:17:20 +0000

    chore: remove server-only libs not used by static export

--- blueprint-management/src/lib/resend.ts
+++
@@ -1,9 +0,0 @@
-import { Resend } from "resend";
-import { requireSecret } from "./env";
-
-export function getResend() {
-  return new Resend(requireSecret("RESEND_API_KEY"));
-}
-
-// Re-export for backward-compat with existing imports
-export { requireAdminToken } from "./auth";
